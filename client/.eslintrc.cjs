77cb45189848d8f0c7a92c92bfe7e80ccd9bffe7
John J Rodriguez <87795271+JohnRodriguezM@users.noreply.github.com>
2024-01-24 21:40:52 -0500

    Add Vite environment file and configuration Delete old App.jsx and main.jsx files Update Vite configuration to use React plugin Add new App.tsx and main.tsx files Add tsconfig.node.json file Update index.html title Add tsconfig.json file Delete App.css file Update package.json dependencies and devDependencies Update ESLint configuration

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -3,18 +3,23 @@ module.exports = {
   env: { browser: true, es2020: true },
   extends: [
     'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
     'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:prettier/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react', 'react-hooks', 'jsx-a11y', 'prettier', 'react-refresh'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    'react/react-in-jsx-scope': 'off', // Not necessary with new JSX Transform introduced in React 17
+    'react/prop-types': 'off', // Not necessary with TypeScript
+    '@typescript-eslint/explicit-module-boundary-types': 'off', // Personal preference
+    'prettier/prettier': ['error', {}, { usePrettierrc: true }], // Includes .prettierrc.js rules
   },
 }
